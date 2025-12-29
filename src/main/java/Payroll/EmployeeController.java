c63a52f6554a4624577be3ade956f6974773b5d8
Kazimlyc <kazimkalayc@hotmail.com>
2025-12-29 20:16:17 +0300

    Add custom exception handling for missing employees
    Introduced EmployeeNotFoundException and EmployeeNotFoundAdvice to handle cases where an employee is not found, returning a 404 response. Updated EmployeeController to throw the new exception. Minor update to PayrollApplication main method signature.

--- src/main/java/Payroll/EmployeeController.java
+++ src/main/java/Payroll/EmployeeController.java
@@ -36,16 +36,19 @@ class EmployeeController {
 
     @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-        return repository.findById(id).map(employee -> {
+        return repository.findById(id)
+                .map(employee -> {
             employee.setName(newEmployee.getName());
             employee.setRole(newEmployee.getRole());;
             return repository.save(employee);
-        }).orElseGet(() -> { return repository.save(newEmployee);
+        })
+                .orElseGet(() -> { return repository.save(newEmployee);
         });
     }
 
